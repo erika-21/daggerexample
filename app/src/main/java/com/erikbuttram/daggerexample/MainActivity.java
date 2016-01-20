1abd997e79b216083c70bd558f2523f74a2385f9
Erik Buttram <erikb@discreteit.com>
2016-01-19 23:21:40 -0800

    not really started yet.

---
+++ app/src/main/java/com/erikbuttram/daggerexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.erikbuttram.daggerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
