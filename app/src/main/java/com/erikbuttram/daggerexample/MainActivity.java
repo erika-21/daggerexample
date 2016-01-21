b0b00b3dbd7bd06db95e8e445ae5f5ea9830a003
Erik Buttram <erikb@discreteit.com>
2016-01-21 09:39:11 -0800

    Dumb view setup.

--- app/src/main/java/com/erikbuttram/daggerexample/MainActivity.java
+++ app/src/main/java/com/erikbuttram/daggerexample/MainActivity.java
@@ -2,6 +2,12 @@ package com.erikbuttram.daggerexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.erikbuttram.daggerexample.model.SomeData;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -13,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Inject DataSource mDataSource;
 
+    private ViewGroup mBoomGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
         ExampleApplication.component(getApplicationContext())
                 .inject(this);
 
+        mBoomGroup = (ViewGroup) findViewById(R.id.boom_container);
+        ViewGroup dataContainer = (ViewGroup)findViewById(R.id.data_container);
+        View button = findViewById(R.id.boom_btn);
+        button.setOnClickListener(mOnClick);
+        for (SomeData sd : mDataSource.getData()) {
+            TextView tv = new TextView(this);
+            tv.setText(String.format("%s %s", sd.mDataName, sd.mDataValue));
+            dataContainer.addView(tv);
+        }
     }
+
+    private View.OnClickListener mOnClick = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            BoomWidget bw = mBoomProvider.get();
+            mBoomGroup.addView(bw);
+        }
+    };
 }
